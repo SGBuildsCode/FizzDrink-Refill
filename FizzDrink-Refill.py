9e4f44cb3063565df3d69115d89f57070ce53d35
SGBuildsCode <95650528+SGBuildsCode@users.noreply.github.com>
2022-03-21 14:28:39 +0000

    Add files via upload

--- FizzDrink-Refill.py
+++ FizzDrink-Refill.py
@@ -1,24 +1,28 @@
-# This file has been extracted from a project in Visual Studio Code and as such the packages within this file (to the best of my knowledge) may not function correctly should other
-# development environments be used.
+from ast import Break
 from typing import Match
+# For testing purposes include a check where the user must enter an integer.
 
-# In future I wish to include a check where the user must enter an integer.
+# This program (in accordance with convention and logic) assumes and requires that the customer must 
+# finish their drink first before they are asked for another. 
+
+
 
 def ChooseDrink():
-  global Wglass 
-  DrinkChc = int(input("""Which Drink Would You Like? 
-  (0) Coca-Cola
-  (1) Fanta
-  (2) 7Up
-  (3) Lucozade
-  (4) Pepsi
-  (5) Dr.Pepper 
-  (6) Nothing
-   """))
-  
- # Within the input procedure/method, utilizing multiline strings this ensures that we can display a vertical menu to the user which will appear exactly as we have written it.  
-  
-  match DrinkChc: 
+  try:
+    global DrinkChc
+    DrinkChc = int(input("""Which Drink Would You Like? 
+    (0) Coca-Cola
+    (1) Fanta
+    (2) 7Up
+    (3) Lucozade
+    (4) Pepsi
+    (5) Dr.Pepper 
+    (6) Nothing
+    """)) 
+    
+    global Wglass
+
+    match DrinkChc: 
           case 0: 
             Wglass = "Coca-Cola" 
             print("Here is Your" +" "+ Wglass + "!")
@@ -37,17 +41,33 @@ def ChooseDrink():
           case 5: 
             Wglass = "Dr Pepper"  
             print("Here is Your" +" "+ Wglass + "!")
+          case 6: 
+            Wglass = None
+           # print("Good Bye! Hope You Have A Nice Day! ")
+            exit
+    if DrinkChc not in range(0,7):
+      Wglass = None
+      print("The Number You Have Entered Is Not In Range!")
+    else:
+      pass
+  except (ValueError,TypeError):
+    Wglass = None
+    print("You have Not Entered A Number!") 
+
+# This is the input procedure/method and as such it will display the string within the input statement in the manner that the user 
+# has written it. 
 
 ChooseDrink()
 
-while Wglass: 
+while Wglass:
   Refill = input("Would You Like Another Drink? Y For Yes; N For No")
   if Refill == "Y":
     ChooseDrink() 
   elif Refill == 'N':
-    Wglass = "" # This statement is included to exit the while loop. In more detail once the user has entered 'N' for no for the purposes of simplicity 
-    exit        # let's assume that they have finished the drinks that they have been served and they no longer wish to have any more (i.e. they are 
-                # surfeited)   
+    Wglass = ""
+    # This statement is included to exit the while loop.
+    exit
+  
 
 
 
